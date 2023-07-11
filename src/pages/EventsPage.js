4a58497f9ae95b11eae1bcdc51bab29c2053342b
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-11 18:39:28 +0530

    error handling in loaders (returning object)

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -3,6 +3,9 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
 	const data = useLoaderData();
+	if (data.isError) {
+		return <p>{data.message}</p>
+	}
 	const events = data.events;
 	return <EventsList events={events} />
 }
@@ -11,6 +14,7 @@ export const eventsLoader = async () => {
 	const response = await fetch('http://localhost:8080/events');
 
 	if (!response.ok) {
+		return { isError: true, message: "Could not fetch events" };
 	} else {
 		//  we can directly return response here, because react has build in support to recognoze response objets and extract data from it
 		return response;
