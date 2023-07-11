ab191d490b58b30fb06847ed1f61cdb76422cb68
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-11 21:44:23 +0530

    throwing responses from loaders and handling them in root error element

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,27 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 import PageContent from "../components/PageContent";
 const ErrorPage = () => {
+	const error = useRouteError();
+	const status = error.status;
+
+	let title = "An error occured";
+	let message = "Something went wrong.";
+
+	if (status === 500) {
+		message = JSON.parse(error.data).message;
+	}
+	if (status === 404) {
+		title = "Not found";
+		message = "Could not find resource page";
+	}
 	return (
-		<PageContent title="An error occured">
-			<p>Something went wrong.</p>
-		</PageContent >
+		<>
+			<MainNavigation />
+			<PageContent title={title}>
+				<p>{message}</p>
+			</PageContent >
+		</>
 	)
 }
 
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -11,7 +11,9 @@ export const eventsLoader = async () => {
 	const response = await fetch('http://localhost:8080/events');
 
 	if (!response.ok) {
-		throw new Error({ message: "could not fetch events" });
+		// throw new Error({ message: "could not fetch events" });
+		throw new Response(JSON.stringify({ message: "Could not fetch events" }),
+			{ status: 500 });
 	} else {
 		//  we can directly return response here, because react has build in support to recognoze response objets and extract data from it
 		return response;
