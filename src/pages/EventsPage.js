052b6ce2624b970d9f9b5b8cff015a6f7095f1c7
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-14 19:19:01 +0530

    deferring data fetching with defer() - suspend rendering until some async data is resolved

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,24 +1,42 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { json, useLoaderData, defer, Await } from "react-router-dom";
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
 	const data = useLoaderData();
 	const events = data.events;
-	return <EventsList events={events} />
+	return (
+		<Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+			<Await resolve={events}>
+				{(loadedEvents) => <EventsList events={loadedEvents} />}
+			</Await>
+		</Suspense>
+	);
 }
 
-export const eventsLoader = async () => {
+async function loadEvents() {
 	const response = await fetch('http://localhost:8080/events');
 
 	if (!response.ok) {
-		// throw new Error({ message: "could not fetch events" });
-		// throw new Response(JSON.stringify({ message: "Could not fetch events" }),
-		// 	{ status: 500 });
-		throw json({ message: "Could not fetch events" }, { status: 500 });
+		// return { isError: true, message: 'Could not fetch events.' };
+		// throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+		//   status: 500,
+		// });
+		throw json(
+			{ message: 'Could not fetch events.' },
+			{
+				status: 500,
+			}
+		);
 	} else {
-		//  we can directly return response here, because react has build in support to recognoze response objets and extract data from it
-		return response;
+		const resData = await response.json();
+		return resData.events;
 	}
 }
 
+export function eventsLoader() {
+	return defer({
+		events: loadEvents(),
+	});
+}
 export default EventsPage;
